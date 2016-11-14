0ab145cd5810334be1f155716ead7105f9b7c561
Thanh Vuong <thanh@userful.com>
2016-11-14 16:47:16 -0700

    work

---
+++ js/userful.js
@@ -0,0 +1,16 @@
+function nag() {
+  alert("got clicked");
+}
+
+// ajax template
+// function login
+function login(username, password) {
+  // $.ajax (url:"http://192.168.120.199:9000/api/session",
+  //         success: function(result) {
+  //
+  //         }});
+}
+
+function showLogin() {
+  $('#mainContent').load('static/login.html');
+}
